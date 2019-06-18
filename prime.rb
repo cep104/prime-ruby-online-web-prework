b81c24c9b0f361aaf15d2fbe12ea56fe881ae962
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-18 15:59:51 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(number)
     array = (2..number-1).to_a
     #turns array of 2..(given number) - 1 : to an array
     array.none? do |num| 
-      number % num == 0
+      number % num.zero?
 
     end
   else
