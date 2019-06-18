3a6f827ca1e3502f1e5ba5c51ad1d4b0f3f1505f
Candice Peters <candicelizabethpeters@gmail.com>
2019-06-18 15:52:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,12 +4,12 @@ def prime?(number)
     #if the given number is greater than 1
     array = (2..number-1).to_a
     #turns array of 2..(given number) - 1 : to an array
-    array.none? do |num| 
+    array.all? do |num| 
       number % num == 0
 
     end
   else
-    false
+    true
   end
 end
 
